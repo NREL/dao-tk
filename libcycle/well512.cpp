b36339b5f508c0178d8e0c4c490f0bb752be2c4e
Alex Zolan <alexander.zolan@nrel.gov>
2018-08-28 16:58:24 -0600

    jettisoned old code in WELL512 RNG class

--- libcycle/well512.cpp
+++ libcycle/well512.cpp
@@ -3,12 +3,6 @@
 #include <strstream>
 #include <fstream>
 
-//std::random_device rd;     // only used once to initialise (seed) engine
-//std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-//std::uniform_int_distribution<int> uni(min,max); // guaranteed unbiased
-//
-//auto random_integer = uni(rng);
-
 /* 
 
 Alex Zolan
