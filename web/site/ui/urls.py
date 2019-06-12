27b01c2bc02d0b171330e9a24b9dc8e3d1b60b5b
Mike Wagner <mike.wagner@nrel.gov>
2019-06-12 13:23:20 -0500

    web app skeleton

--- web/site/ui/urls.py
+++ web/site/ui/urls.py
@@ -4,4 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path(r'dashboard', views.dashboard, name='dashboard'),
+    path(r'outlook', views.outlook, name='outlook'),
+    path(r'planning', views.planning, name='planning'),
+    path(r'system', views.system, name='system'),
+    path(r'configure/event_log', views.configure_event_log, name='event_log' ),
+    path(r'configure/logging', views.configure_logging, name='logging' ),
+    path(r'configure/model_settings', views.configure_model_settings, name='model_settings' ),
+    path(r'configure/sources', views.configure_sources, name='sources' ),
+    path(r'configure/ui_settings', views.configure_ui_settings, name='ui_settings' ),
+    path(r'whatif/interruptions', views.whatif_interruptions, name='interruptions' ),
+    path(r'whatif/resources', views.whatif_resources, name='resources' ),
+    path(r'whatif/weather', views.whatif_weather, name='weather' ),
 ]
--- web/site/ui/views.py
+++ web/site/ui/views.py
@@ -9,3 +9,56 @@ def index(request):
     context = {}
     return render(request, "base.html", context)
 
+def dashboard(request):
+    context = {}
+    return render(request, "dashboard.html", context)
+
+def outlook(request):
+    context = {}
+    return render(request, "outlook.html", context)
+
+def planning(request):
+    context = {}
+    return render(request, "planning.html", context)
+
+def system(request):
+    context = {}
+    return render(request, "system.html", context)
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+
+def configure_sources(request):
+    context = {}
+    return render(request, "configure/sources.html", context)    
+
+def configure_logging(request):
+    context = {}
+    return render(request, "configure/logging.html", context)    
+
+def configure_model_settings(request):
+    context = {}
+    return render(request, "configure/model_settings.html", context)    
+
+def configure_ui_settings(request):
+    context = {}
+    return render(request, "configure/ui_settings.html", context)    
+
+def configure_event_log(request):
+    context = {}
+    return render(request, "configure/event_log.html", context)    
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+
+def whatif_interruptions(request):
+    context = {}
+    return render(request, "whatif/interruptions.html", context)
+
+def whatif_resources(request):
+    context = {}
+    return render(request, "whatif/resources.html", context)
+
+def whatif_weather(request):
+    context = {}
+    return render(request, "whatif/weather.html", context)
